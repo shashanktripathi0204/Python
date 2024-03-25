cf364b312e92776d3905a87fb4650f7da8f06989
Shashank Tripathi <shashanktripathi99@outlook.com>
2024-03-25 23:54:24 +0530

    fileSys Basics

---
+++ fileSystem/main.py
@@ -0,0 +1,21 @@
+# file = open("my_file.txt")
+# contents = file.read()
+# print(contents)
+# file.close()
+
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+
+# w - writing
+# with open("my_file.txt", mode='w') as file:
+#     file.write("New Text.")
+
+# a - append
+with open("my_file.txt", mode='a') as file:
+    file.write("\nNew Text.")
+
+# opening a file in write mode that dosent exist will be created, works only in write mode
+with open("new_file.txt", mode='w') as file:
+    file.write("New Text.")
