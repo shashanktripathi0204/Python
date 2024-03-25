8a282572e00ccf221051d06110a1866671503885
Shashank Tripathi <shashanktripathi99@outlook.com>
2024-03-25 20:34:29 +0530

    high score 1.0

--- snake/scoreboard.py
+++ snake/scoreboard.py
@@ -9,17 +9,21 @@ class Scoreboard(Turtle):
         super().__init__()
         self.score = 0
         self.color("white")
+        self.high_score = 0
         self.penup()
         self.goto(0, 270)
         self.hideturtle()
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score}, High Socre : {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self) -> None:
+        if self.score > self.high_score:
+            self.score = self.high_score
+        self.score = 0
+        return super().reset()
 
     def increase_score(self):
         self.score += 1
--- snake/snake.py
+++ snake/snake.py
@@ -17,6 +17,13 @@ class Snake:
     def create_snake(self):
         for position in STARTING_POSITIONS:
             self.add_segment(position)
+    
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(2000, 2000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
 
     def add_segment(self, position):
         new_segment = Turtle("square")
