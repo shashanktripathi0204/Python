ad19b86d7774afb5f7978443f758979116a3f99b
Shashank Tripathi <shashanktripathi99@outlook.com>
2024-03-26 15:30:42 +0530

    bascis improve

--- fileSystem/bascis1/main.py
+++ fileSystem/bascis1/main.py
@@ -13,9 +13,16 @@
 #     file.write("New Text.")
 
 # a - append
-with open("my_file.txt", mode='a') as file:
-    file.write("\nNew Text.")
+# with open("my_file.txt", mode='a') as file:
+#     file.write("\nNew Text.")
 
 # opening a file in write mode that dosent exist will be created, works only in write mode
-with open("new_file.txt", mode='w') as file:
-    file.write("New Text.")
+# with open("new_file.txt", mode='w') as file:
+#     file.write("New Text.")
+
+# to open a file from desktop location
+# with open("/Users/Shashank Tripathi/Desktop/fileSys.txt", mode='a') as file:
+#     file.write("\nhey its working")
+
+with open("../../../../../ShashankTripathi/Desktop/fileSys.txt", mode='a') as file:
+    file.write("\nhey its working")
