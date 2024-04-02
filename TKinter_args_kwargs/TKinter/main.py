ff79ea36a1a16b60b7d746a0d6598eaaf6cffc2b
Shashank Tripathi <shashanktripathi99@outlook.com>
2024-04-03 00:33:02 +0530

    pack() example

--- TKinter_args_kwargs/TKinter/main.py
+++ TKinter_args_kwargs/TKinter/main.py
@@ -1,31 +1,32 @@
 # import tkinter
 from tkinter import *
 
+def button_clicked():
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text = new_text)
+
 window = Tk()
 window.title("My first GUI Program")
 window.minsize(width=500, height=300)
 
 # label
 my_label = Label(text = "I am a label", font=("Arial",24,"bold"))
+my_label.config(text = "New Text")
 my_label.pack() # without this it will not be visible on the screen, it packs the label to be represented in the screen
 
-my_label["text"] = "New Text" # my_label.config(text = "yay")
+# my_label["text"] = "New Text" # my_label.config(text = "yay")
 
 # Button
-# def button_clicked():
-#     my_label.config(text = "Button Got Clicked")
-# button = Button(text="Click me", command=button_clicked) # command is name of the function without ()
-# button.pack()
+button = Button(text = "Click Me", command = button_clicked) # command is name of the function without ()
+button.pack()
+
 
 # Entry
-
 input = Entry(width=10)
+print(input.get())
 input.pack()
-def button_clicked():
-    new_text = input.get()
-    my_label.config(text = new_text)
-button = Button(text="Click me", command=button_clicked) # command is name of the function without ()
-button.pack()
+
 
 
 
