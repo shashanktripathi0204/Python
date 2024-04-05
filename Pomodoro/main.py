25a085abefafa17c7e08d9890765fd680d79362e
Shashank Tripathi <shashanktripathi99@outlook.com>
2024-04-05 11:27:10 +0530

    updating the timer countdown mechanism

--- Pomodoro/main.py
+++ Pomodoro/main.py
@@ -9,12 +9,28 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+reps = 0 # will keep track of timers
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(13)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    if reps > 8:
+        return
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        timer_label.config(text = "Break", fg = RED)
+    elif reps % 2 != 0:
+        count_down(work_sec)
+        timer_label.config(text="Work", fg = GREEN)
+    else:
+        count_down(short_break_sec)
+        timer_label.config(text="Break", fg = PINK)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
@@ -32,6 +48,8 @@ def count_down(count):
     canvas.itemconfig(timer_text, text = f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
