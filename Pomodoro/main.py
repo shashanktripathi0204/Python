ba7422d489bcaad27f9fa033e5a35b8545d26c11
Shashank Tripathi <shashanktripathi99@outlook.com>
2024-04-05 10:53:17 +0530

    added the start count down functionality

--- Pomodoro/main.py
+++ Pomodoro/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -12,20 +13,37 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(13)
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+def count_down(count):
+    # finding the min and seconds value
+    count_min = math.floor(count/60)
+    count_sec = count % 60
+    # making the starting as mm:00
+    if count_sec == 0:
+        count_sec = "00"
+    elif count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    # print(count)
+    canvas.itemconfig(timer_text, text = f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx = 100, pady = 50, bg = YELLOW)
+
 # taking the size of the image to be used
 canvas = Canvas(width = 200, height = 224, bg = YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file = "tomato.png")
 canvas.create_image(100, 112, image = tomato_img)
-canvas.create_text(100, 130, text = "00:00", fill = "white", font = (FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text = "00:00", fill = "white", font = (FONT_NAME, 35, "bold"))
 canvas.grid(row = 1, column = 1)
-
 
 # Timer
 timer_label = Label(text = "Timer", font=(FONT_NAME,50,"bold"), fg = GREEN, bg = YELLOW)
@@ -36,7 +54,7 @@ tick_label = Label(text = "🗸", font=("Arial",24,"bold"), fg = GREEN, bg = YEL
 tick_label.grid(row = 3, column = 1)
 
 # start_button
-start_button = Button(text = "START", highlightthickness=0)
+start_button = Button(text = "START", highlightthickness=0, command = start_timer)
 start_button.grid(row = 2, column = 0)
 start_button.grid
 
