24f9f1b09e5d361af32e225f6733d03e807ff3d3
Shashank Tripathi <shashanktripathi99@outlook.com>
2024-06-02 16:01:44 +0530

    adding variables to template

--- Django_Project/Blog/views.py
+++ Django_Project/Blog/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 # # Home will be responsible for handling the traffic at the home page of our blog
 # def home(request):
 #     # we return what we want the user to see when he is sent to the route
@@ -11,9 +10,28 @@ from django.http import HttpResponse
 # def about(request):
 #     return HttpResponse('<h1>Blog About</h1>')
 
+posts = [
+    {
+        "author": "Shashank",
+        "title": "Blog Post 1",
+        "content": "First Post Content",
+        "date_posted": "June 2, 2024"
+    },
+    {
+        "author": "Tripathi",
+        "title": "Blog Post 2",
+        "content": "Second Post Content",
+        "date_posted": "June 3, 2024"
+    }
+]
+
+
 def home(request):
-    return render(request, "Blog/home.html")
+    context = {
+        "posts": posts
+    }
+    return render(request, "Blog/home.html", context)
 
 
 def about(request):
-    return render(request, "Blog/about.html")
+    return render(request, "Blog/about.html", {"title": "About"})
