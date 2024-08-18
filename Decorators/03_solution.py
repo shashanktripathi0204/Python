70ee071a84e764be9a14385e0f8fe217d780c1f0
Shashank Tripathi <shashanktripathi99@outlook.com>
2024-08-19 01:12:47 +0530

    Iterator Generators Decorators Question

--- Decorators/03_solution.py
+++ Decorators/03_solution.py
@@ -1,26 +1,28 @@
 import time
-
+from functools import wraps
 
 def cache(func):
     cache_value = {}
-    print(cache_value)
 
+    @wraps(func)
     def wrapper(*args, **kwargs):
-        if args in cache_value:
-            return cache_value[args]
+        # Convert kwargs to a sorted tuple of key-value pairs to ensure immutability
+        kwargs_tuple = tuple(sorted(kwargs.items()))
+        key = (args, kwargs_tuple)
+        if key in cache_value:
+            return cache_value[key]
         result = func(*args, **kwargs)
-        cache_value[args] = result
+        cache_value[key] = result
         return result
 
     return wrapper
-
 
 @cache
 def long_running_function(a, b):
     time.sleep(4)
     return a + b
 
-
-print(long_running_function(2, 3))
-print(long_running_function(2, 3))
-print(long_running_function(4, 8))
+# Testing the function
+print(long_running_function(2, 3))  # This will take some time (4 seconds)
+print(long_running_function(2, 3))  # This will return instantly from the cache
+print(long_running_function(4, 8))  # This will take some time (4 seconds)
